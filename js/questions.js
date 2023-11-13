41cca16292cb47c5ab887d2c716592811da1e0fd
marispitzer <141043002+marispitzer@users.noreply.github.com>
2023-11-13 12:04:51 -0300

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -55,6 +55,98 @@ let questions = [
       "Mercúrio e Saturno"
     ]
   },
+  {
+    numb: 6,
+    question: "em que ano o Brasil foi descoberto?", 
+    answer: "1500"
+    options:" [
+    "1500"
+    "1700"
+    "2020"
+    "1400"
+]
+},
+{
+   numb: 7,
+      question: "Quem foi a primeira pessoa a viajar no Espaço?", 
+    answer: "Yuri Gagarin"
+    options:" [
+    " A cadela Laika"
+    "Yuri Gagarin"
+    "Neil Armstrong"
+    "Marcos Pontes"
+]
+},
+{
+      numb: 8,
+      question: "Qual a montanha mais alta do mundo?", 
+    answer: "Monte Everest"
+    options:" [
+  "Mauna Kea"
+"Dhaulagiri"
+"Monte Chimborazo"
+  "Monte Everest"
+     ]
+},
+{
+   numb: 9,
+      question: "Onde se localiza Machu Picchu?", 
+    answer: "Peru"
+    options:" [
+  Colômbia
+"Peru"
+"China"
+"Bolívia"
+  ]
+},
+{
+  numb: 10,
+      question: "Que país tem o formato de uma bota?", 
+    answer: "italia"
+    options:" [
+ "Butão"
+"Brasil"
+"Portugal"
+"itália"
+   ]
+},
+{
+   numb: 11,
+      question: "Quem inventou a lâmpada?", 
+    answer: "Thomas Edison"
+    options:" [
+  "Graham Bell"
+"Steve Jobs"
+"Thomas Edison"
+"Henry Ford"
+    ]
+},
+{
+ numb: 12,
+      question: "Quanto tempo a Terra demora para dar uma volta completa em torno dela mesma?", 
+    answer: "Aproximadamente 24 horas"
+    options:" [
+  "Aproximadamente 24 horas"
+"365 dias"
+"7 dias"
+"365 ou 366 dias"
+    ]
+},
+{
+   numb: 13,
+      question: "A que temperatura a água ferve?", 
+    answer: "100 ºC."
+    options:" [
+  "100 ºC."
+"-10 ºC"
+"0 ºC"
+"200 ºC"
+    ]
+},
+{
+  
+    
+      
   // you can uncomment the below codes and make duplicate as more as you want to add question
   // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
 
